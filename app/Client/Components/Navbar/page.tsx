5dd75b6fdcb4766ed4cf60727d8a0ba1bdbb770c
prabha63 <yuviprabha6363@gmail.com>
2026-03-29 13:55:29 +0530

    Frondend updated

--- app/Client/Components/Navbar/page.tsx
+++ app/Client/Components/Navbar/page.tsx
@@ -117,7 +117,7 @@ const handleHome = () => {
               >
                 
                 <MenuItem onClick={handleHome} >Home</MenuItem>
-                {!user && AuthStatus!==500?<MenuItem onClick={handleLogin}>Login</MenuItem>:null}
+                {AuthStatus === 500 ? null : !user ?  <MenuItem onClick={handleLogin}>Login</MenuItem>:null}
                 <MenuItem onClick={handleAccount}>Account</MenuItem>
               </Menu>
             </div>
